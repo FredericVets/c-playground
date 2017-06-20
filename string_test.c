da5a0b418a00eee68cb4025496d7f45673e79cf4
Frederic Vets <frederic.vets@gmail.com>
2017-06-20 02:10:23 +0200

    cleaned up and added comments to the string_test file.
    Signed-off-by: Frederic Vets <frederic.vets@gmail.com>

--- string_test.c
+++ string_test.c
@@ -1,56 +1,73 @@
 #include <stdio.h>
+#include <string.h>
 #include <ctype.h>
 
-void arrays_in_disguise() {
-    // Arrays are pointers in disguise.
-    char name[] = "John Doe";
-    char *ptr = "John Doe";
-
-    printf("sizeof name : %lu\n", sizeof(name));
-    printf("sizeof ptr : %lu\n", sizeof(ptr));
-    printf("Name : %s\n", ptr);
-
-    while (*ptr) {
-        putchar(*ptr);
-        putchar('\n');
-
-        ptr++;
+static void itterate_as_pointer(char *string) {
+    // while (*string) is a shorthan for while (*string != '\0'), the null-byte.
+    while (*string) {
+        putchar(*string);
+        string++;
     }
+
+    putchar('\n');
 }
 
-void convert_to_upper(char *string) {
-    // In both cases, the character '\0' (null byte) will cause the loop to exit.
-    // The pointer way;
+static void itterate_as_array(char *string) {
+    for (int i=0; string[i]; i++)
+        putchar(string[i]);
+
+    putchar('\n');
+}
+
+static void convert_to_upper(char *string) {
     while (*string) {
         *string = toupper(*string);
         string++;
     }
-
-    return;
-
-    // The array way;
-    for (int i = 0; string[i]; i++)
-        string[i] = toupper(string[i]);
 }
 
 int main() {
-    arrays_in_disguise();
+    /*
+    A string literal (the formal term for a double-quoted string in C source) can be used in two slightly different ways:
 
-    char string[] = "shouting!";
-    char c = 'c';
+    1. As the initializer for an array of char, as in the declaration of char a[] , it specifies the initial values of
+        the characters in that array (and, if necessary, its size).
+        Stored on the stack. So MODIFIABLE!
+    2. Anywhere else, it turns into an unnamed, static array of characters, and this unnamed array may be stored in
+        read-only memory, and which therefore cannot necessarily be modified. In an expression context, the array is
+        converted at once to a pointer, as usual (see section 6), so the second declaration initializes p to point to
+        the unnamed array's first element.
+        Stored in the rodata segment. So NOT MODIFIABLE!
 
-    convert_to_upper(string);
-    convert_to_upper(&c);
+    See : https://stackoverflow.com/questions/1335786/c-differences-between-char-pointer-and-array
+    Great and interesting comments are mentioned in it.
+    */
 
-    char *char_p = &c;
-    printf("address 1 : %p\n", char_p);
-    printf("address 2 : %p\n", char_p[1]);
-    printf("value 1 : %c\n", char_p[0]);
-    printf("value 2 : %i\n", char_p[1]);
+    // Case 1.
+    char array_string[] = "This string is assigned to an array.";
+    // Case 2.
+    char *pointer_string = "This string is assigned to a pointer.";
 
-    printf("convert_to_upper result : %s\n", string);
-    printf("convert_to_upper result : %c\n", c);
+    printf("Address of array_string = %p\n", array_string);
+    printf("strlen(array_string) = %lu\n", strlen(array_string));
+    printf("sizeof array_string = %lu\n\n", sizeof array_string); // IMPORTANT !
+
+    printf("Address of pointer_string = %p\n", pointer_string);
+    printf("strlen(pointer_string) = %lu\n", strlen(pointer_string));
+    printf("sizeof pointer_string = %lu\n\n", sizeof pointer_string);
+
+    itterate_as_pointer(array_string);
+    itterate_as_array(array_string);
+
+    itterate_as_pointer(pointer_string);
+    itterate_as_array(pointer_string);
+
+    convert_to_upper(array_string);
+    printf("array_string in uppercase : %s\n", array_string);
+
+    // This will cause an ERROR because read-only memory is attempted to be written to.
+//    convert_to_upper(pointer_string);
+//    printf("array_string in uppercase : %s\n", pointer_string);
 
     return 0;
 }
-
